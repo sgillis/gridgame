20ec9c955016f9bea5953a7aec9b5cdabdf0a044
San Gillis <gillis.san@gmail.com>
2013-10-21 21:22:46 +0200

    move player 2

--- js/game.js
+++ js/game.js
@@ -10,32 +10,52 @@ function Game(){
         if($event.keyCode == 119){
             // w
             if(this.players[0][1] > 0){
-                this.players[0][1] -= 1
+                this.players[0][1] -= 1;
             }
         } else if($event.keyCode == 97){
             // a
             if(this.players[0][0] > 0){
-                this.players[0][0] -= 1
+                this.players[0][0] -= 1;
             }
         } else if($event.keyCode == 115){
             // s
             if(this.players[0][1] < this.max_range){
-                this.players[0][1] += 1
+                this.players[0][1] += 1;
             }
         } else if($event.keyCode == 100){
             // d
             if(this.players[0][0] < this.max_range){
-                this.players[0][0] += 1
+                this.players[0][0] += 1;
+            }
+        } else if($event.keyCode == 105){
+            // w
+            if(this.players[1][1] > 0){
+                this.players[1][1] -= 1;
+            }
+        } else if($event.keyCode == 106){
+            // a
+            if(this.players[1][0] > 0){
+                this.players[1][0] -= 1;
+            }
+        } else if($event.keyCode == 107){
+            // s
+            if(this.players[1][1] < this.max_range){
+                this.players[1][1] += 1;
+            }
+        } else if($event.keyCode == 108){
+            // d
+            if(this.players[1][0] < this.max_range){
+                this.players[1][0] += 1;
             }
         }
-    }
+    };
 
     this.containsPlayer = function(player_num, x, y){
         return this.players[player_num][0] == x && this.players[player_num][1] == y;
-    }
+    };
 
     this.updatePlayerLocation = function(x, y){
         this.players[0][0] = x;
         this.players[0][1] = y;
-    }
+    };
 }
